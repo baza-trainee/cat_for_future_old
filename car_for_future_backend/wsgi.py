a3d107a7d0122b95fe325668eb1d4608c8e8dac2
Ruslan Poghorilyi <tgvyhb389@gmail.com>
2023-08-29 13:17:48 +0300

    init

---
+++ car_for_future_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for car_for_future_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_for_future_backend.settings')
+
+application = get_wsgi_application()
