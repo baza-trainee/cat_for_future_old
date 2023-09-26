b75b000b1ecf666bbdc29e3054702e34081afc98
Ruslan <tgvyhb389@gmail.com>
2023-09-26 13:57:10 +0300

    Fixed typos in subscription serializer

--- cat_for_future_backend/cat_for_future_backend/subscriptions/api/serializers.py
+++ cat_for_future_backend/cat_for_future_backend/subscriptions/api/serializers.py
@@ -6,5 +6,5 @@ from cat_for_future_backend.subscriptions.models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
-        fields = ["user", "cat", "created_at"]
-        readonly_fields = ["created_at"]
+        fields = ["user", "cat", "date_created"]
+        read_only_fields = ["date_created"]
