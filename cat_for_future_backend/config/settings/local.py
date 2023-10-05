9525d8253ea7feba06f43204d35060b8708c04cd
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2023-10-05 18:51:08 +0300

    Update local.py

--- cat_for_future_backend/config/settings/local.py
+++ cat_for_future_backend/config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # CACHES
 # ------------------------------------------------------------------------------
